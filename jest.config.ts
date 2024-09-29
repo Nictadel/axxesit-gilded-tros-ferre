384cb4ea34705e0ecff744ba5ad5dc888851d95d
Ferre Hamblok <ferre@hamblok.be>
2024-09-29 17:08:31 +0200

    Update dependencies to make Jest exclude dist folder

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type {Config} from 'jest';
+
+const config: Config = {
+  verbose: true,
+  preset: 'ts-jest',
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    "^.+\\.(js|jsx)$": "babel-jest",
+  }
+};
+
+export default config;
