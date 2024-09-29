ec5a3c9321a0ca6a5995be5d90f662a9d76adf77
Ferre Hamblok <ferre@hamblok.be>
2024-09-29 18:04:17 +0200

    Add missing rule for good wine

--- src/models/improving-item.ts
+++ src/models/improving-item.ts
@@ -1,10 +1,16 @@
-import { MAXIMUM_QUALITY, MINIMUM_QUALITY } from '../constants'
+import { MAXIMUM_QUALITY, MINIMUM_QUALITY, SELL_IN_EXPIRED } from '../constants'
 import clamp from '../helpers/clamp'
 import { InventoryItem } from './inventory-item'
 
 export class ImprovingItem extends InventoryItem {
   updateQuality(): void {
     this.sellIn--
-    this.quality = clamp(this.quality + 1, MINIMUM_QUALITY, MAXIMUM_QUALITY)
+    this.quality = clamp(
+      this.sellIn < SELL_IN_EXPIRED
+        ? this.quality + 2
+        : this.quality + 1,
+      MINIMUM_QUALITY,
+      MAXIMUM_QUALITY,
+    )
   }
 }
