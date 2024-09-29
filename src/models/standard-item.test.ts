e130e0ebb3c31ca4f25a47c64aebf00a376324b7
Ferre Hamblok <ferre@hamblok.be>
2024-09-29 16:15:48 +0200

    Rework standard item with clamp function

--- src/models/standard-item.test.ts
+++ src/models/standard-item.test.ts
@@ -1,35 +1,48 @@
 import { StandardItem } from './standard-item';
+import { MAXIMUM_QUALITY, MINIMUM_QUALITY } from '../constants';
+
 
 describe('Standard Item', () => {
-    it('should decrease quality by 1 when sellIn is greater than 0', () => {
-        const item = new StandardItem('Ring of Cleansening Code', 10, 20);
-        item.updateQuality();
-        expect(item.sellIn).toBe(9);
-        expect(item.quality).toBe(19);
-    })
+  it('should decrease quality by 1 when sellIn is greater than 0', () => {
+    const item = new StandardItem('Test Item', 10, 20);
+    item.updateQuality();
+    expect(item.quality).toBe(19);
+  });
 
-    it('should decrease quality by 2 when sellIn is less than 0', () => {
-        const item = new StandardItem('Ring of Cleansening Code', 0, 20);
-        item.updateQuality();
-        expect(item.sellIn).toBe(-1);
-        expect(item.quality).toBe(18);
-    })
+  it('should decrease quality by 2 when sellIn is less than or equal to 0', () => {
+    const item = new StandardItem('Test Item', 0, 20);
+    item.updateQuality();
+    expect(item.quality).toBe(18);
+  });
 
-    it('should not decrease quality below 0', () => {
-        const item = new StandardItem('Ring of Cleansening Code', 10, 0);
-        item.updateQuality();
-        expect(item.quality).toBe(0);
-    })
+  it('should not decrease quality below 0', () => {
+    const item = new StandardItem('Test Item', 10, 0);
+    item.updateQuality();
+    expect(item.quality).toBe(0);
+  });
 
-    it('should decrease sellIn by 1', () => {
-        const item = new StandardItem('Ring of Cleansening Code', 10, 20);
-        item.updateQuality();
-        expect(item.sellIn).toBe(9);
-    })
+  it('should decrease sellIn by 1', () => {
+    const item = new StandardItem('Test Item', 10, 20);
+    item.updateQuality();
+    expect(item.sellIn).toBe(9);
+  });
 
-    it ('should still decrease sellIn by 1 when sellIn is less than 0', () => {
-        const item = new StandardItem('Ring of Cleansening Code', -2, 20);
-        item.updateQuality();
-        expect(item.sellIn).toBe(-3);
-    })
-})
+  it('should still decrease sellIn by 1 when sellIn is less than 0', () => {
+    const item = new StandardItem('Test Item', -2, 20);
+    item.updateQuality();
+    expect(item.sellIn).toBe(-3);
+  });
+
+  it('should clamp quality to minimum quality when quality is below minimum', () => {
+    const item = new StandardItem('Test Item', 10, MINIMUM_QUALITY - 1);
+    item.updateQuality();
+    expect(item.quality).toBe(MINIMUM_QUALITY);
+  });
+
+  it('should clamp quality to maximum quality when quality is above maximum', () => {
+    const item = new StandardItem('Test Item', 10, MAXIMUM_QUALITY + 10);
+    item.updateQuality();
+    expect(item.quality).toBe(MAXIMUM_QUALITY);
+  });
+});
+
--- src/models/standard-item.ts
+++ src/models/standard-item.ts
@@ -1,32 +1,16 @@
 import { MAXIMUM_QUALITY, MINIMUM_QUALITY, SELL_IN_EXPIRED } from "../constants";
+import clamp from "../helpers/clamp";
 import { InventoryItem } from "./inventory-item";
 
 export class StandardItem extends InventoryItem {
 
-  private DEGRADE_RATE = 1;
-
-  private degradeQuality(amount: number): void {
-    if (this.quality - amount < MINIMUM_QUALITY) {
-      this.quality = MINIMUM_QUALITY;
-    }
-    else {
-      this.quality -= amount;
-    }
-  }
-
   updateQuality(): void {
-
-    if (this.quality > MAXIMUM_QUALITY){
-      this.quality = MAXIMUM_QUALITY;
-    }
-
     this.sellIn--;
 
-    if (this.sellIn <= SELL_IN_EXPIRED) {
-      this.degradeQuality(this.DEGRADE_RATE * 2);
-    }
-    else {
-      this.degradeQuality(this.DEGRADE_RATE);
-    }
+    this.quality = clamp(
+      this.sellIn <= SELL_IN_EXPIRED
+        ? this.quality - 2
+        : this.quality - 1,
+      MINIMUM_QUALITY, MAXIMUM_QUALITY);
   }
 }
