24f46f278db94095710e9deb007ed28a1cc1e6a6
Ferre Hamblok <ferre@hamblok.be>
2024-09-29 15:52:09 +0200

    Rewrite intial test with jest .each()

--- test/gilded-tros.spec.ts
+++ test/gilded-tros.spec.ts
@@ -1,15 +1,15 @@
-import {Item} from '../src/item';
-import {GildedTros} from '../src/gilded-tros';
+import { Item } from '../src/item';
+import { GildedTros } from '../src/gilded-tros';
 
-describe('GildedTrosTest', () => {
-    const items: Item[] = [new Item('foo', 0, 0)];
-    const app: GildedTros = new GildedTros(items);
-    app.updateQuality();
-    expect(app.items[0].name).toEqual('fixme');
-});
+// describe('GildedTrosTest', () => {
+//     const items: Item[] = [new Item('foo', 0, 0)];
+//     const app: GildedTros = new GildedTros(items);
+//     app.updateQuality();
+//     expect(app.items[0].name).toEqual('fixme');
+// });
 
 
-describe('The original functionality', () => {
+describe('App should work with original data', () => {
 
     const items: Item[] = [
         new Item('Ring of Cleansening Code', 10, 20),
@@ -26,55 +26,66 @@ describe('The original functionality', () => {
         new Item('Ugly Variable Names', 3, 6)
     ];
 
-    it('should update correctly for the initially known working items', () => {
+    const goodItems = items.slice(0, 8)
+    const app: GildedTros = new GildedTros(goodItems);
 
-        const goodItems = items.slice(0, 8)
-
-        //Initial State
-
-        expect(goodItems[0].toString()).toEqual('Ring of Cleansening Code, 10, 20');
-        expect(goodItems[1].toString()).toEqual('Good Wine, 2, 0');
-        expect(goodItems[2].toString()).toEqual('Elixir of the SOLID, 5, 7');
-        expect(goodItems[3].toString()).toEqual('B-DAWG Keychain, 0, 80');
-        expect(goodItems[4].toString()).toEqual('B-DAWG Keychain, -1, 80');
-        expect(goodItems[5].toString()).toEqual('Backstage passes for Re:Factor, 15, 20');
-        expect(goodItems[6].toString()).toEqual('Backstage passes for Re:Factor, 10, 49');
-        expect(goodItems[7].toString()).toEqual('Backstage passes for HAXX, 5, 49');
-
-        const app: GildedTros = new GildedTros(goodItems);
-
-        //Day 1
-        app.updateQuality();
-
-        expect(goodItems[0].toString()).toEqual('Ring of Cleansening Code, 9, 19');
-        expect(goodItems[1].toString()).toEqual('Good Wine, 1, 1');
-        expect(goodItems[2].toString()).toEqual('Elixir of the SOLID, 4, 6');
-        expect(goodItems[3].toString()).toEqual('B-DAWG Keychain, 0, 80');
-        expect(goodItems[4].toString()).toEqual('B-DAWG Keychain, -1, 80');
-        expect(goodItems[5].toString()).toEqual('Backstage passes for Re:Factor, 14, 21');
-        expect(goodItems[6].toString()).toEqual('Backstage passes for Re:Factor, 9, 50');
-
-        //Day 2
-        app.updateQuality();
-
-        expect(goodItems[0].toString()).toEqual('Ring of Cleansening Code, 8, 18');
-        expect(goodItems[1].toString()).toEqual('Good Wine, 0, 2');
-        expect(goodItems[2].toString()).toEqual('Elixir of the SOLID, 3, 5');
-        expect(goodItems[3].toString()).toEqual('B-DAWG Keychain, 0, 80');
-        expect(goodItems[4].toString()).toEqual('B-DAWG Keychain, -1, 80');
-        expect(goodItems[5].toString()).toEqual('Backstage passes for Re:Factor, 13, 22');
-        expect(goodItems[6].toString()).toEqual('Backstage passes for Re:Factor, 8, 50');
-        expect(goodItems[7].toString()).toEqual('Backstage passes for HAXX, 3, 50');
-
-        //Day 3
-        app.updateQuality();
-        expect(goodItems[0].toString()).toEqual('Ring of Cleansening Code, 7, 17');
-        expect(goodItems[1].toString()).toEqual('Good Wine, -1, 4');
-        expect(goodItems[2].toString()).toEqual('Elixir of the SOLID, 2, 4');
-        expect(goodItems[3].toString()).toEqual('B-DAWG Keychain, 0, 80');
-        expect(goodItems[4].toString()).toEqual('B-DAWG Keychain, -1, 80');
-        expect(goodItems[5].toString()).toEqual('Backstage passes for Re:Factor, 12, 23');
-        expect(goodItems[6].toString()).toEqual('Backstage passes for Re:Factor, 7, 50');
-        expect(goodItems[7].toString()).toEqual('Backstage passes for HAXX, 2, 50');
+    it.each([
+        {
+            // Day 0 - Initial State
+            expected: [
+                'Ring of Cleansening Code, 10, 20',
+                'Good Wine, 2, 0',
+                'Elixir of the SOLID, 5, 7',
+                'B-DAWG Keychain, 0, 80',
+                'B-DAWG Keychain, -1, 80',
+                'Backstage passes for Re:Factor, 15, 20',
+                'Backstage passes for Re:Factor, 10, 49',
+                'Backstage passes for HAXX, 5, 49'
+            ]
+        },
+        {   
+            // Day 1
+            expected: [
+                'Ring of Cleansening Code, 9, 19',
+                'Good Wine, 1, 1',
+                'Elixir of the SOLID, 4, 6',
+                'B-DAWG Keychain, 0, 80',
+                'B-DAWG Keychain, -1, 80',
+                'Backstage passes for Re:Factor, 14, 21',
+                'Backstage passes for Re:Factor, 9, 50',
+                'Backstage passes for HAXX, 4, 50'
+            ]
+        },
+        {
+            // Day 2
+            expected: [
+                'Ring of Cleansening Code, 8, 18',
+                'Good Wine, 0, 2',
+                'Elixir of the SOLID, 3, 5',
+                'B-DAWG Keychain, 0, 80',
+                'B-DAWG Keychain, -1, 80',
+                'Backstage passes for Re:Factor, 13, 22',
+                'Backstage passes for Re:Factor, 8, 50',
+                'Backstage passes for HAXX, 3, 50'
+            ]
+        },
+        {
+            // Day 3
+            expected: [
+                'Ring of Cleansening Code, 7, 17',
+                'Good Wine, -1, 4',
+                'Elixir of the SOLID, 2, 4',
+                'B-DAWG Keychain, 0, 80',
+                'B-DAWG Keychain, -1, 80',
+                'Backstage passes for Re:Factor, 12, 23',
+                'Backstage passes for Re:Factor, 7, 50',
+                'Backstage passes for HAXX, 2, 50'
+            ]
+        }
+    ])('update the items correctly for day %#', ({ expected }) => {
+        goodItems.forEach((item, index) => {
+            expect(item.toString()).toEqual(expected[index]);
+        })
+        app.updateQuality()
     })
 })
